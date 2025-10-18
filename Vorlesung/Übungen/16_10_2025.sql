d15143f256919e4a946cb9f13166371ee2492189
karkessler <karsten@kkessler.de>
2025-10-18 10:31:53 +0200

    Uppercase SQL keywords in exercises

--- Vorlesung/Übungen/16_10_2025.sql
+++ Vorlesung/Übungen/16_10_2025.sql
@@ -1,66 +1,66 @@
-drop table IF EXISTS Hoeren;
-drop table IF EXISTS Student;
-drop table IF EXISTS Hochschule;
-drop table IF EXISTS Vorlesung;
+DROP TABLE IF EXISTS Hoeren;
+DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS Hochschule;
+DROP TABLE IF EXISTS Vorlesung;
 
-create table Hochschule
-(HSNr integer primary key,
-Bezeichnung varchar(50) not null,
-Ort varchar(50),
-PLZ integer unique);
+CREATE TABLE Hochschule
+(HSNr INTEGER PRIMARY KEY,
+Bezeichnung VARCHAR(50) NOT NULL,
+Ort VARCHAR(50),
+PLZ INTEGER UNIQUE);
 
-create table Vorlesung
-(VorlNr integer primary key,
-Titel varchar(50),
-SWS integer);
+CREATE TABLE Vorlesung
+(VorlNr INTEGER PRIMARY KEY,
+Titel VARCHAR(50),
+SWS INTEGER);
 
-create table Student
-(MatNr integer primary key,
-Name varchar(50) not null,
-Semester integer,
-HS integer not null,
+CREATE TABLE Student
+(MatNr INTEGER PRIMARY KEY,
+Name VARCHAR(50) NOT NULL,
+Semester INTEGER,
+HS INTEGER NOT NULL,
 FOREIGN KEY (HS) REFERENCES Hochschule(HSNr));
 
-create table Hoeren
-(MatNr integer,
-VorlNr integer,
-foreign key Hoeren_Student_FK (MatNr) references Student (MatNr),
-foreign key Hoeren_Vorlesung_FK (VorlNr) references Vorlesung (VorlNr),
-primary key (MatNr, VorlNr)
+CREATE TABLE Hoeren
+(MatNr INTEGER,
+VorlNr INTEGER,
+FOREIGN KEY Hoeren_Student_FK (MatNr) REFERENCES Student (MatNr),
+FOREIGN KEY Hoeren_Vorlesung_FK (VorlNr) REFERENCES Vorlesung (VorlNr),
+PRIMARY KEY (MatNr, VorlNr)
 );
 
 
-insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
-values (11,'DHBW Stuttgart', 'Stuttgart', 70245);
-insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
-values (22,'DHBW Heilbronn', 'Heilbronn', 76584);
-insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
-values (33,'TU Darmstadt', 'Darmstadt', 60125);
-insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
-values (44,'Hochschule Bayern', 'München', 87312);
-insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
-values (55,'Hochschule Franken', 'Nürnberg', 90285);
-insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
-values (66,'Hochschule Hessen', 'Frankfurt', 60123);
+INSERT INTO Hochschule (HSNr, Bezeichnung, Ort, PLZ)
+VALUES (11,'DHBW Stuttgart', 'Stuttgart', 70245);
+INSERT INTO Hochschule (HSNr, Bezeichnung, Ort, PLZ)
+VALUES (22,'DHBW Heilbronn', 'Heilbronn', 76584);
+INSERT INTO Hochschule (HSNr, Bezeichnung, Ort, PLZ)
+VALUES (33,'TU Darmstadt', 'Darmstadt', 60125);
+INSERT INTO Hochschule (HSNr, Bezeichnung, Ort, PLZ)
+VALUES (44,'Hochschule Bayern', 'München', 87312);
+INSERT INTO Hochschule (HSNr, Bezeichnung, Ort, PLZ)
+VALUES (55,'Hochschule Franken', 'Nürnberg', 90285);
+INSERT INTO Hochschule (HSNr, Bezeichnung, Ort, PLZ)
+VALUES (66,'Hochschule Hessen', 'Frankfurt', 60123);
 
-insert into Vorlesung (VorlNr, Titel, SWS)
-values (123, 'Funktionalanalysis', 4);
-insert into Vorlesung (VorlNr, Titel, SWS)
-values (456, 'Maßtheorie', 6);
-insert into Vorlesung (VorlNr, Titel, SWS)
-values (789, 'Partielle DGL', 4);
+INSERT INTO Vorlesung (VorlNr, Titel, SWS)
+VALUES (123, 'Funktionalanalysis', 4);
+INSERT INTO Vorlesung (VorlNr, Titel, SWS)
+VALUES (456, 'Maßtheorie', 6);
+INSERT INTO Vorlesung (VorlNr, Titel, SWS)
+VALUES (789, 'Partielle DGL', 4);
 
-insert into Student (MatNr, Name, Semester, HS)
-values (1,'Paul', 1, 11);
-insert into Student (MatNr, Name, Semester, HS)
-values (2,'Schmitt ', 8, 22  );
-insert into Student (MatNr, Name, Semester, HS)
-values (3,'Meier', 4, 22);
-insert into Student (MatNr, Name, Semester, HS)
-values (4,'Meier', 4, 33);
+INSERT INTO Student (MatNr, Name, Semester, HS)
+VALUES (1,'Paul', 1, 11);
+INSERT INTO Student (MatNr, Name, Semester, HS)
+VALUES (2,'Schmitt ', 8, 22  );
+INSERT INTO Student (MatNr, Name, Semester, HS)
+VALUES (3,'Meier', 4, 22);
+INSERT INTO Student (MatNr, Name, Semester, HS)
+VALUES (4,'Meier', 4, 33);
 
-insert into Hoeren (MatNr, VorlNr) values (4, 123);
+INSERT INTO Hoeren (MatNr, VorlNr) VALUES (4, 123);
 
-select * from Student;
-select * from Hochschule;
-select * from Vorlesung;
+SELECT * FROM Student;
+SELECT * FROM Hochschule;
+SELECT * FROM Vorlesung;
