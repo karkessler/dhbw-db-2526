99a64ed461239468c22c6438585ab8fbe75ee9ec
karkessler <karsten@kkessler.de>
2025-10-18 09:29:09 +0200

    Rename student-related tables to singular form

--- Vorlesung/Übungen/16_10_2025.sql
+++ Vorlesung/Übungen/16_10_2025.sql
@@ -1,66 +1,66 @@
 drop table IF EXISTS Hoeren;
-drop table IF EXISTS studenten;
-drop table IF EXISTS Hochschulen;
-drop table IF EXISTS Vorlesungen;
+drop table IF EXISTS Student;
+drop table IF EXISTS Hochschule;
+drop table IF EXISTS Vorlesung;
 
-create table Hochschulen
+create table Hochschule
 (HSNr integer primary key,
 Bezeichnung varchar(50) not null,
 Ort varchar(50),
 PLZ integer unique);
 
-create table Vorlesungen
+create table Vorlesung
 (VorlNr integer primary key,
 Titel varchar(50),
 SWS integer);
 
-create table Studenten
+create table Student
 (MatNr integer primary key,
 Name varchar(50) not null,
 Semester integer,
 HS integer not null,
-FOREIGN KEY (HS) REFERENCES Hochschulen(HSNr));
+FOREIGN KEY (HS) REFERENCES Hochschule(HSNr));
 
 create table Hoeren
 (MatNr integer,
 VorlNr integer,
-foreign key Hoeren_Studenten_FK (MatNr) references Studenten (MatNr),
-foreign key Hoeren_Vorlesungen_FK (VorlNr) references Vorlesungen (VorlNr),
+foreign key Hoeren_Student_FK (MatNr) references Student (MatNr),
+foreign key Hoeren_Vorlesung_FK (VorlNr) references Vorlesung (VorlNr),
 primary key (MatNr, VorlNr)
 );
 
 
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (11,'DHBW Stuttgart', 'Stuttgart', 70245);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (22,'DHBW Heilbronn', 'Heilbronn', 76584);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (33,'TU Darmstadt', 'Darmstadt', 60125);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (44,'Hochschule Bayern', 'München', 87312);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (55,'Hochschule Franken', 'Nürnberg', 90285);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (66,'Hochschule Hessen', 'Frankfurt', 60123);
 
-insert into Vorlesungen (VorlNr, Titel, SWS)
+insert into Vorlesung (VorlNr, Titel, SWS)
 values (123, 'Funktionalanalysis', 4);
-insert into Vorlesungen (VorlNr, Titel, SWS)
+insert into Vorlesung (VorlNr, Titel, SWS)
 values (456, 'Maßtheorie', 6);
-insert into Vorlesungen (VorlNr, Titel, SWS)
+insert into Vorlesung (VorlNr, Titel, SWS)
 values (789, 'Partielle DGL', 4);
 
-insert into Studenten (MatNr, Name, Semester, HS)
+insert into Student (MatNr, Name, Semester, HS)
 values (1,'Paul', 1, 11);
-insert into Studenten (MatNr, Name, Semester, HS)
+insert into Student (MatNr, Name, Semester, HS)
 values (2,'Schmitt ', 8, 22  );
-insert into Studenten (MatNr, Name, Semester, HS)
+insert into Student (MatNr, Name, Semester, HS)
 values (3,'Meier', 4, 22);
-insert into Studenten (MatNr, Name, Semester, HS)
+insert into Student (MatNr, Name, Semester, HS)
 values (4,'Meier', 4, 33);
 
 insert into Hoeren (MatNr, VorlNr) values (4, 123);
 
-select * from studenten;
-select * from Hochschulen;
-select * from Vorlesungen;
+select * from Student;
+select * from Hochschule;
+select * from Vorlesung;
--- Vorlesung/Übungen/Datenbanken4Übungen_1.sql
+++ Vorlesung/Übungen/Datenbanken4Übungen_1.sql
@@ -1,86 +1,86 @@
 -------------------------------------------------
 --- Szenario 1:
---- 1. Studenten besuchen Hochschule (n:1)
---- 2. Professoren halten Vorlesungen (1:n)
---- 3. Studenten hören Vorlesungen (n:m)
+--- 1. Student besuchen Hochschule (n:1)
+--- 2. Professoren halten Vorlesung (1:n)
+--- 3. Student hören Vorlesung (n:m)
 --- 4. Assistenen haben Vorgesetzte (n:1)
 --- 5. Generalisierung: Doktorand is_a Angestellter 
 --- 6. Student kann beliebig viele Autos besitzen, Auto hat entweder keinen oder einen Besitzer
 -------------------------------------------------
 
 -- -----------------------------------------------
-create table Studenten 
+create table Student 
 (MatNr integer not null,
 Name varchar(50) not null,
 Semester integer);
 -- ------------------------
-insert into Studenten (MatNr, Name, Semester)
+insert into Student (MatNr, Name, Semester)
 values (1,'Paul', 1);
-insert into Studenten (MatNr, Name, Semester)
+insert into Student (MatNr, Name, Semester)
 values (2,'Schmitt ', 8);
-insert into Studenten (MatNr, Name, Semester)
+insert into Student (MatNr, Name, Semester)
 values (3,'Meier', 4);
 -- ------------------------
-insert into Studenten (MatNr, Name, Semester)
+insert into Student (MatNr, Name, Semester)
 values (3,'Meier', 4); -- Problem: Redundanzen möglich
 --------------------------
-alter table Studenten add constraint Id_Studenten primary key (MatNr);
+alter table Student add constraint Id_Student primary key (MatNr);
 --------------------------
-delete from Studenten where Name = 'Meier' -- Problem: wie kann ich Studenten Meier löschen?
--- ALTER TABLE Studenten ADD ID INT AUTO_INCREMENT PRIMARY KEY;
--- DELETE FROM Studenten WHERE ID = 5;
--- oder: DELETE FROM Studenten WHERE Name = 'Meier' LIMIT 1;
+delete from Student where Name = 'Meier' -- Problem: wie kann ich Student Meier löschen?
+-- ALTER TABLE Student ADD ID INT AUTO_INCREMENT PRIMARY KEY;
+-- DELETE FROM Student WHERE ID = 5;
+-- oder: DELETE FROM Student WHERE Name = 'Meier' LIMIT 1;
 -- ------------------------
-drop table Studenten;
+drop table Student;
 -- ------------------------
-create table Studenten 
+create table Student 
 (MatNr integer primary key, 
 Name varchar(50) not null,
 Semester integer);
 -- ------------------------
-create table Hochschulen 
+create table Hochschule 
 (HSNr integer primary key,
 Bezeichnung varchar(50) not null,
 Ort varchar(50),
 PLZ integer unique); -- darf in der Tabelle nur 1x vorhanden sein
 -- ------------------------
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (11,'DHBW Stuttgart', 'Stuttgart', 70245);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (22,'DHBW Heilbronn', 'Heilbronn', 76584);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (33,'TU Darmstadt', 'Darmstadt', 60125);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (44,'Hochschule Bayern', 'München', 87312);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (55,'Hochschule Franken', 'Nürnberg', 90285);
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (66,'Hochschule Hessen', 'Frankfurt', 60123);
 -- ------------------------
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (22,'TU München', 'München', 83124); -- geht ja/nein?
 -- ------------------------
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (77,'TU München', 'München', 87312); -- geht ja/nein?
 -- ------------------------
-insert into Hochschulen (HSNr, Bezeichnung, Ort, PLZ)
+insert into Hochschule (HSNr, Bezeichnung, Ort, PLZ)
 values (77,'TU München', 'München', 87313); -- geht ja/nein?
 -- ------------------------
 -- --> Problem: wie implementiere ich die Beziehung 'besucht'?
-alter table Studenten add constraint Studenten_Hochschulen_FK foreign key (besuchtHS) references Hochschulen (HSNr) -- immer auf der n-Seite, sonst Redundanzen
+alter table Student add constraint Student_Hochschule_FK foreign key (besuchtHS) references Hochschule (HSNr) -- immer auf der n-Seite, sonst Redundanzen
 -- ------------------------
-alter table Studenten add besuchtHS integer not null; 
--- -- eigentlich: alter table Studenten modify besuchtHS integer null (da 1:n und nicht c:n)
+alter table Student add besuchtHS integer not null; 
+-- -- eigentlich: alter table Student modify besuchtHS integer null (da 1:n und nicht c:n)
 -- ------------------------
 -- im Editor auf 11/22/33 setzen (Apply klicken)
 -- ------------------------
-alter table Studenten add constraint Studenten_Hochschulen_FK foreign key (besuchtHS) references Hochschulen (HSNr) 
+alter table Student add constraint Student_Hochschule_FK foreign key (besuchtHS) references Hochschule (HSNr) 
 -- ------------------------
-insert into Studenten (MatNr, Name, Semester)
+insert into Student (MatNr, Name, Semester)
 values (4,'Peter', 5); -- Problem kein Default   
-insert into Studenten (MatNr, Name, Semester, besuchtHS)
+insert into Student (MatNr, Name, Semester, besuchtHS)
 values (4,'Peter', 5, 99);
-insert into Studenten (MatNr, Name, Semester, besuchtHS)
+insert into Student (MatNr, Name, Semester, besuchtHS)
 values (4,'Peter', 5, 33);    
 -- ------------------------
 create table Professoren 
@@ -96,30 +96,30 @@ values (13,'Heinz', 'Boss', 001);
 insert into Professoren (PersNr, Name, Rang, Raum)
 values (13,'Heinz', 'Boss', 002);    
 -- ------------------------
-drop table Vorlesungen;
+drop table Vorlesung;
 -- ------------------------
-create table Vorlesungen 
+create table Vorlesung 
 (VorlNr integer primary key,
 Titel varchar(50),
 SWS integer,
 gelesenVon integer not null); -- für die Beziehung 'lesen'
 -- ------------------------
-alter table Vorlesungen add constraint Vorlesungen_Professoren_FK foreign key (gelesenVon) references Professoren (PersNr)
+alter table Vorlesung add constraint Vorlesung_Professoren_FK foreign key (gelesenVon) references Professoren (PersNr)
 --------------------------
-insert into Vorlesungen (VorlNr, Titel, SWS, gelesenVon)
+insert into Vorlesung (VorlNr, Titel, SWS, gelesenVon)
 values (123, 'Funktionalanalysis', 4, 13);    
-insert into Vorlesungen (VorlNr, Titel, SWS, gelesenVon)
+insert into Vorlesung (VorlNr, Titel, SWS, gelesenVon)
 values (456, 'Maßtheorie', 6, 13);    
-insert into Vorlesungen (VorlNr, Titel, SWS, gelesenVon)
+insert into Vorlesung (VorlNr, Titel, SWS, gelesenVon)
 values (456, 'Maßtheorie', 6, 12);    
 -- ------------------------
-insert into Vorlesungen (VorlNr, Titel, SWS, gelesenVon)
+insert into Vorlesung (VorlNr, Titel, SWS, gelesenVon)
 values (789, 'Partielle DGL', 4, 12);     
 -- ------------------------
 create table Hören 
 (MatNr integer, VorlNr integer,
-foreign key Hören_Studenten_FK (MatNr) references Studenten (MatNr),
-foreign key Hören_Vorlesungen_FK (VorlNr) references Vorlesungen (VorlNr)); -- Nicht hierarchische Beziehung in Mapping-Tabelle auslagern
+foreign key Hören_Student_FK (MatNr) references Student (MatNr),
+foreign key Hören_Vorlesung_FK (VorlNr) references Vorlesung (VorlNr)); -- Nicht hierarchische Beziehung in Mapping-Tabelle auslagern
 -- ------------------------
 insert into Hören (MatNr, VorlNr) values (4, 123);
 insert into Hören (MatNr, VorlNr) values (4, 123);
@@ -128,8 +128,8 @@ insert into Hören (MatNr, VorlNr) values (4, 123); -- Problem: kann man wiederh
 create table Hören 
 (MatNr integer, 
 VorlNr integer,
-foreign key Hören_Studenten_FK (MatNr) references Studenten (MatNr),
-foreign key Hören_Vorlesungen_FK (VorlNr) references Vorlesungen (VorlNr),
+foreign key Hören_Student_FK (MatNr) references Student (MatNr),
+foreign key Hören_Vorlesung_FK (VorlNr) references Vorlesung (VorlNr),
 primary key (MatNr, VorlNr)
 );
 
@@ -189,7 +189,7 @@ insert into Autos (AutoNr, Marke, Typ, Baujahr) values (91, 'Fiat', 'Punto', 201
 create table Fahrzeughalter -- da c:mc nicht hierarchische Beziehung
 (MatNr integer,
 AutoNr integer,
-foreign key Fahrzeughalter_MatrNummer_FK (MatNr) references Studenten (MatNr),
+foreign key Fahrzeughalter_MatrNummer_FK (MatNr) references Student (MatNr),
 foreign key Fahrzeughalter_Autonummer_FK (AutoNr) references Autos (AutoNr)
 )
 -- ------------------------
@@ -209,7 +209,7 @@ drop table Fahrzeughalter;
 create table Fahrzeughalter
 (MatNr integer,
 AutoNr integer,
-foreign key Fahrzeughalter_MatrNummer_FK (MatNr) references Studenten (MatNr),
+foreign key Fahrzeughalter_MatrNummer_FK (MatNr) references Student (MatNr),
 foreign key Fahrzeughalter_Autonummer_FK (AutoNr) references Autos (AutoNr),
 primary key (AutoNr)
 )
